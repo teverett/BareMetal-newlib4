884b55345e8058e77d3be6a0ca6319a5bcb0501c
jack andrews <effbiae@gmail.com>
2024-11-27 15:32:29 +1100

    test.c does a bit more

--- test.c
+++ test.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-int main()
+int main(int argc, char *argv[])
 {
+	struct tm *local;
+	time_t t;
+	int i=0;
+
 	printf("NewLib Test Application\n=======================\n");
+
+	// Test time
+	t = time(NULL);
+	local = localtime(&t);
+	printf("Local time and date: %s\n", asctime(local));
+
+	printf("%s %d\n", "printf-example:", 1234);
 
 	return 0;
 }
